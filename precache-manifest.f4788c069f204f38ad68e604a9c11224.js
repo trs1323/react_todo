34de7b6611060944f20ca865f5d7613b5ce0c951
trs1323 <45774674+trs1323@users.noreply.github.com>
2019-08-31 11:21:59 -0700

    Updates

---
+++ precache-manifest.f4788c069f204f38ad68e604a9c11224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb24ab79c1181edd7f5e1f145044cd3",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "ced5991205c1f24a0e29",
+    "url": "/react_todo/static/css/main.fff8a9a3.chunk.css"
+  },
+  {
+    "revision": "158ceb21e337d7808287",
+    "url": "/react_todo/static/js/2.2fb05ead.chunk.js"
+  },
+  {
+    "revision": "ced5991205c1f24a0e29",
+    "url": "/react_todo/static/js/main.93d042dd.chunk.js"
+  },
+  {
+    "revision": "fe35fca0fea852899367",
+    "url": "/react_todo/static/js/runtime~main.db2a12cb.js"
+  }
+]);
